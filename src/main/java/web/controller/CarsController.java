06c33c2a266b32205dea2432f28757e538cdc37a
Abubakar Gusaev <abakar.gusaev@gmail.com>
2024-04-22 18:28:01 +0300

    di with interface

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 @Controller
 public class CarsController {
-    private final CarServiceImpl carService;
+    private final CarService carService;
 
-    public CarsController(CarServiceImpl carService) {
+    public CarsController(CarService carService) {
         this.carService = carService;
     }
 
